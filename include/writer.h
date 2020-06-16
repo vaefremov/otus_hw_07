1ae85d326cd97ca5064eba7e455864b9ebe2c557
Vladimir Efremov <vefremov@gmail.com>
2020-06-16 22:26:46 +0200

    Writers class hierarchy introduced

--- include/writer.h
+++ include/writer.h
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <exception>
 #include <chrono>
+#include <fstream>
 
 #include "iobserver.h"
 #include "command_reader.h"
@@ -13,21 +14,16 @@
 namespace OTUS
 {
 
-class OstreamWriter: public IObserver 
+class AbstractExecutor: public IObserver
 {
     public:
-    static std::shared_ptr<OstreamWriter> create(std::string const& name, std::ostream& out, size_t block_sz, OTUS::CommandReader& reader)
-    {
-        auto ptr = std::make_shared<OstreamWriter>(name, out, block_sz);
-        reader.subscribe(ptr);
-        return ptr;
-    }
-    OstreamWriter() = delete;
-    OstreamWriter(OstreamWriter const&) = delete;
-    OstreamWriter& operator=(OstreamWriter const&) = delete;
-    OstreamWriter(std::string const& name, std::ostream& out, size_t block_sz): m_name(name), m_out(out), m_block_sz(block_sz) {}
+    AbstractExecutor() = delete;
+    AbstractExecutor(AbstractExecutor const&) = delete;
+    AbstractExecutor& operator=(AbstractExecutor const&) = delete;
+    virtual ~AbstractExecutor() = default;
+    AbstractExecutor(std::string const& name, size_t block_sz): m_name{name}, m_block_sz{block_sz} {}
 
-    void update(Event const& ev)
+    virtual void update(Event const& ev) override
     {
         std::cerr << m_name << event_type_name(ev.m_type) << ev.m_payload << std::endl;
         if(ev.m_type == EventType::COMMAND)
@@ -44,8 +40,9 @@ class OstreamWriter: public IObserver
             m_block_start_tm = 0;
         }
     }
-
-    bool update_state(EventType t)
+    protected:
+    virtual void execute() = 0;
+    virtual bool update_state(EventType t)
     {
         switch (t)
         {
@@ -67,7 +64,36 @@ class OstreamWriter: public IObserver
         return false;
     }
 
-    void execute()
+    std::string m_name;
+    size_t m_block_sz = 1;
+    std::queue<std::string> m_commands;
+    size_t m_nesting_level = 0;
+    long long m_block_start_tm = 0;
+};
+
+class OstreamWriter: public AbstractExecutor 
+{
+    public:
+    static std::shared_ptr<OstreamWriter> create(std::string const& name, std::ostream& out, size_t block_sz, OTUS::CommandReader& reader)
+    {
+        auto ptr = std::make_shared<OstreamWriter>(name, out, block_sz);
+        reader.subscribe(ptr);
+        return ptr;
+    }
+    OstreamWriter() = delete;
+    OstreamWriter(OstreamWriter const&) = delete;
+    OstreamWriter& operator=(OstreamWriter const&) = delete;
+    OstreamWriter(std::string const& name, std::ostream& out, size_t block_sz): AbstractExecutor(name, block_sz), m_out(out) {}
+
+
+    std::string make_log_file_name()
+    {
+        return "bulk" + std::to_string(m_block_start_tm) + ".log";
+    }
+
+    private:
+
+    void execute() override
     {
         if(m_commands.empty())
         {
@@ -82,19 +108,50 @@ class OstreamWriter: public IObserver
         m_out << std::endl;
     }
     
-    std::string make_log_file_name()
+    
+    std::ostream& m_out;
+};
+
+class FilesWriter: public AbstractExecutor
+{
+    public:
+    static std::shared_ptr<FilesWriter> create(std::string const& name, size_t block_sz, OTUS::CommandReader& reader)
+    {
+        auto ptr = std::make_shared<FilesWriter>(name, block_sz);
+        reader.subscribe(ptr);
+        return ptr;
+    }
+    FilesWriter() = delete;
+    FilesWriter(FilesWriter const&) = delete;
+    FilesWriter& operator=(FilesWriter const&) = delete;
+    FilesWriter(std::string const& name, size_t block_sz): AbstractExecutor(name, block_sz) {}
+
+
+    void execute() override
+    {
+        m_out.open(make_log_file_name());
+        if(m_commands.empty())
+        {
+            return;
+        }
+        m_out << "bulk:";
+        while(!m_commands.empty())
+        {
+            m_out << " " << m_commands.front();
+            m_commands.pop();
+        }
+        m_out << std::endl;
+        m_out.close();
+        std::cerr << "Log written to " << make_log_file_name() << std::endl;
+    }
+    
+    std::string make_log_file_name() const
     {
         return "bulk" + std::to_string(m_block_start_tm) + ".log";
     }
-
     private:
-    
-    std::string m_name;
-    std::ostream& m_out;
-    size_t m_block_sz;
-    size_t m_nesting_level = 0;
-    std::queue<std::string> m_commands;
-    long long m_block_start_tm;
+    std::ofstream m_out;
 };
+
 
 }
